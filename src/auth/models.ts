b41229ff577cbb1c45300aefaaf6a370704375eb
Kamilo Vasquez <killo.blanco@gmail.com>
2024-01-31 23:28:40 -0500

    🗃️ Update auth models and add relations

--- src/auth/models.ts
+++ src/auth/models.ts
@@ -1,5 +1,5 @@
-import { sql } from "drizzle-orm";
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { relations, sql } from "drizzle-orm";
+import { sqliteTable, text, integer, blob, primaryKey } from "drizzle-orm/sqlite-core";
 
 export const authNonce = sqliteTable('auth_nonce', {
     uid: text('uid', { mode: 'text' }).primaryKey(),
@@ -12,3 +12,37 @@ export const auth = sqliteTable('auth', {
     createdAt: integer('created_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
     updatedAt: integer('updated_at', { mode: 'timestamp_ms' }).default(sql`CURRENT_TIMESTAMP`),
 })
+
+export const authRelations = relations(auth, ({ many }) => ({
+    authCredentials: many(authToCredentials)
+}))
+
+export const authCredential = sqliteTable('auth_credential', {
+    uid: text('uid').primaryKey(),
+    credentialID: text('credential_id').notNull(),
+    credentialPublicKey: text('credential_public_key').notNull(),
+    counter: integer('counter').notNull(),
+    credentialDeviceType: text('credential_device_type').notNull(),
+    credentialBackedUp: integer('credential_backed_up', { mode: 'boolean' }).notNull(),
+    transports: text('transports', { mode: 'json' }).notNull(),
+})
+
+export const authCredentialRelations = relations(authCredential, ({ many }) => ({
+    authCredentials: many(authToCredentials)
+}))
+
+export const authToCredentials = sqliteTable('auth_credentials', {
+    auth: text('auth_uid').notNull().references(() => auth.uid),
+    credential: text('credential_uid').notNull().references(() => authCredential.uid),
+}, (table) => ({ pk: primaryKey({ columns: [table.auth, table.credential] }) }))
+
+export const authToCredentialRelations = relations(authToCredentials, ({ one }) => ({
+    auth: one(auth, {
+        fields: [authToCredentials.auth],
+        references: [auth.uid]
+    }),
+    credential: one(authCredential, {
+        fields: [authToCredentials.credential],
+        references: [authCredential.uid]
+    })
+}))
