aebfa768573a85c15ed85eb0af3fa13b9d527265
Kamilo Vasquez <killo.blanco@gmail.com>
2024-01-31 23:30:32 -0500

    ✨ Add functions to get temporary nonce and save new user credentials

--- src/auth/repo.ts
+++ src/auth/repo.ts
@@ -1,6 +1,7 @@
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as models from './models';
+import { eq } from 'drizzle-orm';
 
 export const setTmpNonce = async (uid: string, nonce: string) => {
     const client = createClient({
@@ -10,4 +11,47 @@ export const setTmpNonce = async (uid: string, nonce: string) => {
     const db = drizzle(client, { schema: models })
 
     await db.insert(models.authNonce).values({ uid, nonce })
+}
+
+export const getTmpNonce = async (uid: string) => {
+    const client = createClient({
+        url: import.meta.env.TURSO_URL,
+        authToken: import.meta.env.TURSO_TOKEN
+    })
+    const db = drizzle(client, { schema: models })
+
+    const nonce = await db.query.authNonce.findFirst({
+        where: (auth, { eq }) => eq(auth.uid, uid)
+    });
+
+    await db.delete(models.authNonce).where(eq(models.authNonce.uid, uid))
+    return nonce?.nonce;
+}
+
+export const saveNewUserCredentials = async (credential: {
+    aid: string,
+    credentialID: Uint8Array,
+    credentialPublicKey: Uint8Array,
+    counter: number,
+    credentialDeviceType: string,
+    credentialBackedUp: boolean,
+    transports: string[]
+}) => {
+    const client = createClient({
+        url: import.meta.env.TURSO_URL,
+        authToken: import.meta.env.TURSO_TOKEN
+    })
+    const db = drizzle(client, { schema: models })
+
+    const credUUID = globalThis.crypto.randomUUID();
+
+    await db.insert(models.authCredential).values({
+        uid: credUUID,
+        credentialID: credential.credentialID.toString(),
+        credentialPublicKey: credential.credentialPublicKey.toString(),
+        counter: credential.counter,
+        credentialDeviceType: credential.credentialDeviceType,
+        credentialBackedUp: credential.credentialBackedUp,
+        transports: credential.transports,
+    });
 }
