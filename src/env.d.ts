34bfa5bdc1e50333a4dad28be643a582d9c9da88
Kamilo Vasquez <killo.blanco@gmail.com>
2024-01-31 22:23:21 -0500

    🏷️ Add ImportMetaEnv and ImportMeta interfaces to env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,15 @@
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly TURSO_URL: string;
+    readonly TURSO_TOKEN: string;
+    readonly AUTH_SECURE_SOURCE: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
 declare namespace App {
     interface Locals {
-        challenge: string;
     }
 }
